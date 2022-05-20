75cbdf6adb5dede68d04e3da03bd6c64345f996f
M Ray <rayma89@icloud.com>
2022-05-20 08:57:55 -0400

    add FizzBuzz coding excercise

---
+++ fizz-buzz.py
@@ -0,0 +1,13 @@
+def fizz_buzz(max_value):
+    for num in range(1, max_value + 1):
+        if num % 3 == 0 and num % 5 == 0:
+            print('FizzBuzz')
+        elif num % 3 == 0:
+            print('Fizz')
+        elif num % 5 == 0:
+            print('Buzz')
+        else:
+            print(num)            
+
+
+fizz_buzz(501)
